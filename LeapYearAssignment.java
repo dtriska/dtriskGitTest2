b4cedc22452936a5a06fa113c476afbb3302eccf
Derek Triska <dtriska21@yahoo.com>
2022-09-05 13:42:02 -0700

    first commit

---
+++ LeapYearAssignment.java
@@ -0,0 +1,28 @@
+
+package leapyearassignment;
+import java.util.Scanner;
+
+
+public class LeapYearAssignment {
+
+    
+    public static void main(String[] args) {
+        Scanner scnr=new Scanner(System.in);
+        System.out.println("Enter the year in question: ");
+        int userYear = scnr.nextInt();
+        
+        System.out.println(userYear);
+        
+        if ((userYear %100 == 0)&&(userYear %400 == 0))
+        {
+            System.out.println(userYear + " - is Leap Year");
+        }
+        else if((userYear % 100 > 0)&& (userYear % 4 == 0)){
+            System.out.println(userYear + " - is Leap Year");
+        }
+        else{
+            System.out.println(userYear + " - is not Leap Year");
+        }
+    }
+    //change test
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#dtriskGitTest2
